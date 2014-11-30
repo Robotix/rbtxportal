08cc160750fbf202eab9d6f75481d0ac4dd7d85d
Aditya Narayan <narayanaditya95@gmail.com>
2014-11-30 06:47:00 +0530

    Initial commit: Working database and Team app

---
+++ teams/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+
+class Participant(models.Model):
+    firstName = models.CharField(max_length=50)
+    lastName = models.CharField(max_length=50)
+    mobileNo = models.CharField(max_length=13)
+    emailID= models.EmailField()
+    college = models.CharField(max_length=100)
+
+class Team(models.Model):
+    id = models.AutoField(primary_key=True)
+    event = models.CharField(max_length=3)
+    participants = models.ManyToManyField(Participant)
+
+    def getTeamID(self):
+        return '%s-%s' %(self.event, self.id)
